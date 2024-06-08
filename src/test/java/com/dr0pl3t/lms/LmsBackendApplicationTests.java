234656662b0344badd79c6f2a59cb62e2857b012
Dr0pl3t <165064738+Dr0pl3t@users.noreply.github.com>
2024-06-08 16:06:44 +0200

    Adding the project

---
+++ src/test/java/com/dr0pl3t/lms/LmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dr0pl3t.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
